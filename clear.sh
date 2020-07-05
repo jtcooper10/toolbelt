b3e486b6978a559623613c884d9cebffda6c51ac
Josh C <jtcooper90@gmail.com>
2020-07-05 11:41:29 -0400

    Safer clear script, does not rm if backup was not successful

--- clear.sh
+++ clear.sh
@@ -2,6 +2,14 @@
 
 # TODO: Pull from a list of config folders rather than hard-coding them
 
+# Add any new config locations to the functions below
+function backupcfg {
+    cp -r ~/.config/nvim ./backup/nvim
+}
+function clearcfg {
+    rm -rf ~/.config/nvim
+}
+
 # Prompt the user if they want a backup or not, only accepts Y/N
 while [ -z $BACKUP ]; do
     printf "Do you want to create a backup copy of your configration files? (Y/N): "
@@ -17,9 +25,8 @@ done
 # If a backup was requested, create a ./backup/ folder and copy all files over
 if [ $BACKUP == "Y" ]; then
     mkdir -p ./backup
-    cp -r ~/.config/nvim ./backup/nvim
+    backupcfg && clearcfg
+elif [ $BACKUP == "N" ]; then
+    clearcfg
 fi
-
-# Clear out the configuration files
-rm -rf ~/.config/nvim
 
